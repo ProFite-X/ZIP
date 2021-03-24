092e94b252264b2c5192493e8450a19fa91add04
ProFite-X <65878288+ProFite-X@users.noreply.github.com>
2021-03-24 16:58:26 +0800

    Add files via upload

---
+++ ZIP/ZIP/Common.h
@@ -0,0 +1,11 @@
+#pragma  once
+
+#include<string>
+using std::string;
+typedef unsigned char uch;
+typedef unsigned short ush;
+typedef unsigned long long ulg;
+
+const static ush MAX_MATCH = 258;
+const static ush MIN_MATCH = 3;
+const static ush WSIZE = 0x8000;
